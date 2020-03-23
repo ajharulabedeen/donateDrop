5270ce301f772d4599632273d2b0db343868cc32
ajharul abedeen <ajharulabedeen@gmail.com>
2020-03-23 20:02:09 +0600

    f-home : home html code removed, code copied from alumniLTE!

--- font_end/src/app/home/home.component.ts
+++ font_end/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TopnavbarComponent } from '../layout/topnavbar/topnavbar.component';
-import { AuthService } from '../auth/auth.service';
-import { Subscription } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {TopnavbarComponent} from '../layout/topnavbar/topnavbar.component';
+import {AuthService} from '../auth/auth.service';
+import {Subscription} from 'rxjs';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,8 +12,10 @@ export class HomeComponent implements OnInit {
 
   isAuthenticated = false;
   private userSub: Subscription;
-  date : string;
-  constructor(private authService: AuthService) { }
+  date: string;
+
+  constructor(private authService: AuthService) {
+  }
 
   top = new TopnavbarComponent();
   myMsg = 'Hello World!';
@@ -20,9 +23,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     window.dispatchEvent(new Event('resize'));
     document.body.className = 'hold-transition skin-blue sidebar-mini';
-    console.log("Home onInit()");
+    console.log('Home onInit()');
     this.userSub = this.authService.user.subscribe(user => {
-    console.log("Auth Subscriber home:");
+      console.log('Auth Subscriber home:');
       this.isAuthenticated = !!user;
       console.log(!user);
       console.log(!!user);
@@ -35,17 +38,33 @@ export class HomeComponent implements OnInit {
     document.body.className = '';
   }
 
+  // start : for tab in font end.
+  public openCity(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(' active', '');
+    }
+    document.getElementById(tabName).style.display = 'block';
+    evt.currentTarget.className += ' active';
+  }
+  // end : for tab in font end.
+
 
 }//class
 
 
 // activeHome() {
-  //   if (this.home) {
-  //     this.home = false;
-  //     this.top.logFalse();
-  //   }
-  //   else if (this.home == false) {
-  //     this.home = true;
-  //   }
-  //   // if(LoginComponent.loggedIn){
-  // }//activeHome
+//   if (this.home) {
+//     this.home = false;
+//     this.top.logFalse();
+//   }
+//   else if (this.home == false) {
+//     this.home = true;
+//   }
+//   // if(LoginComponent.loggedIn){
+// }//activeHome
