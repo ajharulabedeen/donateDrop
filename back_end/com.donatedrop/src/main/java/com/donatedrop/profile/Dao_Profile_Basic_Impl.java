6e708a39d11b014c69c7f46cb12fd33ab849d143
ajharul abedeen <ajharulabedeen@gmail.com>
2020-03-25 19:56:36 +0600

    b-profilebasic : Dao interface impl class skelton created!

---
+++ back_end/com.donatedrop/src/main/java/com/donatedrop/profile/Dao_Profile_Basic_Impl.java
@@ -0,0 +1,35 @@
+package com.donatedrop.profile;
+
+public class Dao_Profile_Basic_Impl implements Dao_Profile_Basic_I {
+
+    @Override
+    public String save(ProfileBasics profileBasic) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(ProfileBasics profileBasicUpdate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ProfileBasics findOne(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ProfileBasics findOneByUser(String userId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String basicExist(String userId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
--- back_end/com.donatedrop/src/test/java/com/donatedrop/article_test/Test_ArticleDao.java
+++ back_end/com.donatedrop/src/test/java/com/donatedrop/article_test/Test_ArticleDao.java
@@ -1,64 +1,64 @@
-package com.donatedrop.article_test;
-
-import com.donatedrop.articles.Articles;
-import com.donatedrop.articles.Dao_Article_I;
-import com.donatedrop.util.DateUtil;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-///**
-// *
-// * @author Sajib
-// */
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class Test_ArticleDao {
-
-    @Autowired
-    Dao_Article_I articlePersistence_I;
+//package com.donatedrop.article_test;
 //
-//    
-
-    public Test_ArticleDao() {
-    }
-//    
-
-    /**
-     * passing date : 22-12-2019 Sun
-     */
-    @Test
-    public void test_post_Article() {
-//        Articles articles = new Articles(15,"articleSubject", "articleContent", "articleType");
-        Articles articles = new Articles("articleSubject", "articleContent", "articleType", 2, DateUtil.getDate().toString());
-        articlePersistence_I.post_Article(articles);
-//        Dao_Article_Impl article_Impl = new Dao_Article_Impl();
-//        article_Impl.post_Article(articles);
-    }
-//    
-
-    /**
-     * @see passed date : 22-12-2019 Sun
-     */
+//import com.donatedrop.articles.Articles;
+//import com.donatedrop.articles.Dao_Article_I;
+//import com.donatedrop.util.DateUtil;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+/////**
+//// *
+//// * @author Sajib
+//// */
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class Test_ArticleDao {
+//
+//    @Autowired
+//    Dao_Article_I articlePersistence_I;
+////
+////
+//
+//    public Test_ArticleDao() {
+//    }
+////
+//
+//    /**
+//     * passing date : 22-12-2019 Sun
+//     */
 //    @Test
-    public void test_delete_Article() {
-        Integer articleId = 6;
-        articlePersistence_I.delete_Article(articleId);
-    }
-//    
-
-    @Test
-    public void test_edit_Article() {
-        Articles articles = new Articles("articleSubject", "articleContent", "articleType", 2, DateUtil.getDate().toString());
-//        Articles articles = new Articles();
-        articles.setArticleId(new Integer(6));
-        articles.setArticleSubject("Test");
-        articles.setCategory("test, Lie!");
-//        articles.;
-        articlePersistence_I.edit_Article(articles);
-    }
+//    public void test_post_Article() {
+////        Articles articles = new Articles(15,"articleSubject", "articleContent", "articleType");
+//        Articles articles = new Articles("articleSubject", "articleContent", "articleType", 2, DateUtil.getDate().toString());
+//        articlePersistence_I.post_Article(articles);
+////        Dao_Article_Impl article_Impl = new Dao_Article_Impl();
+////        article_Impl.post_Article(articles);
+//    }
+////
 //
+//    /**
+//     * @see passed date : 22-12-2019 Sun
+//     */
+//    @Test
+//    public void test_delete_Article() {
+//        Integer articleId = 6;
+//        articlePersistence_I.delete_Article(articleId);
+//    }
+////
 //
-}//Test_ArticlePersistence
+////    @Test
+//    public void test_edit_Article() {
+//        Articles articles = new Articles("articleSubject", "articleContent", "articleType", 2, DateUtil.getDate().toString());
+////        Articles articles = new Articles();
+//        articles.setArticleId(new Integer(6));
+//        articles.setArticleSubject("Test");
+//        articles.setCategory("test, Lie!");
+////        articles.;
+//        articlePersistence_I.edit_Article(articles);
+//    }
+////
+////
+//}//Test_ArticlePersistence
