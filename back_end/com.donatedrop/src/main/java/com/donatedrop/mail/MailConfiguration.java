391c81b2c39a27594670a3fe06fc9eb157f165b0
ajharul abedeen <ajharulabedeen@gmail.com>
2020-04-20 03:55:59 +0600

    b-mail : mail send success.
    Mail sending tested, have to refactor the code.

---
+++ back_end/com.donatedrop/src/main/java/com/donatedrop/mail/MailConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.donatedrop.mail;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+/**
+ *
+ * @author G7
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.donatedrop.*"})
+public class MailConfiguration {
+
+    @Bean
+    public SimpleMailMessage templateSimpleMessage() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText(
+                "This is the test email template for your email:\n%s\n");
+        return message;
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("gub.cse.files@gmail.com");
+        mailSender.setPassword("(+JGda2993+)");
+        System.out.println("\n\n--Pass : " + mailSender.getPassword());
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
