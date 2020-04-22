c7986894d3d500ca6e11dc9f4f231def29c90366
ajharul abedeen <ajharulabedeen@gmail.com>
2020-04-22 12:07:33 +0600

    f-profilebasic : checkBox UI fix!

--- font_end/src/app/profile/basic/basic.model.ts
+++ font_end/src/app/profile/basic/basic.model.ts
@@ -1,159 +1,176 @@
 export class Basic {
-  private id: string;
-  private name: string;
-  private birth_date: string;
-  private gender: string;
-  private blood_Group: string;
-  private available: string;
+  private  id:string;
+  private  userId:string;
+  private  name:string;
+  private  birthDate:string;
+  private  care_of:string;
+  private  gender:string;
+  private  maritalStatus:string;
+  private  profession:string;
+  private  blood_Group:string;
+  private  available:string;
 
-  private care_of: string;
-  private profession: string;
-  private user_id: string;
 
+    /**
+     * Getter $id
+     * @return {string}
+     */
+	public get $id(): string {
+		return this.id;
+	}
 
-  /**
-   * Getter $id
-   * @return {string}
-   */
-  public get $id(): string {
-    return this.id;
-  }
+    /**
+     * Getter $userId
+     * @return {string}
+     */
+	public get $userId(): string {
+		return this.userId;
+	}
 
-  /**
-   * Getter $available
-   * @return {string}
-   */
-  public get $available(): string {
-    return this.available;
-  }
+    /**
+     * Getter $name
+     * @return {string}
+     */
+	public get $name(): string {
+		return this.name;
+	}
 
-  /**
-   * Getter $birth_date
-   * @return {string}
-   */
-  public get $birth_date(): string {
-    return this.birth_date;
-  }
+    /**
+     * Getter $birthDate
+     * @return {string}
+     */
+	public get $birthDate(): string {
+		return this.birthDate;
+	}
 
-  /**
-   * Getter $blood_Group
-   * @return {string}
-   */
-  public get $blood_Group(): string {
-    return this.blood_Group;
-  }
+    /**
+     * Getter $care_of
+     * @return {string}
+     */
+	public get $care_of(): string {
+		return this.care_of;
+	}
 
-  /**
-   * Getter $care_of
-   * @return {string}
-   */
-  public get $care_of(): string {
-    return this.care_of;
-  }
+    /**
+     * Getter $gender
+     * @return {string}
+     */
+	public get $gender(): string {
+		return this.gender;
+	}
 
-  /**
-   * Getter $gender
-   * @return {string}
-   */
-  public get $gender(): string {
-    return this.gender;
-  }
+    /**
+     * Getter $maritalStatus
+     * @return {string}
+     */
+	public get $maritalStatus(): string {
+		return this.maritalStatus;
+	}
 
-  /**
-   * Getter $name
-   * @return {string}
-   */
-  public get $name(): string {
-    return this.name;
-  }
+    /**
+     * Getter $profession
+     * @return {string}
+     */
+	public get $profession(): string {
+		return this.profession;
+	}
 
-  /**
-   * Getter $profession
-   * @return {string}
-   */
-  public get $profession(): string {
-    return this.profession;
-  }
+    /**
+     * Getter $blood_Group
+     * @return {string}
+     */
+	public get $blood_Group(): string {
+		return this.blood_Group;
+	}
 
-  /**
-   * Getter $user_id
-   * @return {string}
-   */
-  public get $user_id(): string {
-    return this.user_id;
-  }
+    /**
+     * Getter $available
+     * @return {string}
+     */
+	public get $available(): string {
+		return this.available;
+	}
 
-  /**
-   * Setter $id
-   * @param {string} value
-   */
-  public set $id(value: string) {
-    this.id = value;
-  }
+    /**
+     * Setter $id
+     * @param {string} value
+     */
+	public set $id(value: string) {
+		this.id = value;
+	}
 
-  /**
-   * Setter $available
-   * @param {string} value
-   */
-  public set $available(value: string) {
-    this.available = value;
-  }
+    /**
+     * Setter $userId
+     * @param {string} value
+     */
+	public set $userId(value: string) {
+		this.userId = value;
+	}
 
-  /**
-   * Setter $birth_date
-   * @param {string} value
-   */
-  public set $birth_date(value: string) {
-    this.birth_date = value;
-  }
+    /**
+     * Setter $name
+     * @param {string} value
+     */
+	public set $name(value: string) {
+		this.name = value;
+	}
 
-  /**
-   * Setter $blood_Group
-   * @param {string} value
-   */
-  public set $blood_Group(value: string) {
-    this.blood_Group = value;
-  }
+    /**
+     * Setter $birthDate
+     * @param {string} value
+     */
+	public set $birthDate(value: string) {
+		this.birthDate = value;
+	}
 
-  /**
-   * Setter $care_of
-   * @param {string} value
-   */
-  public set $care_of(value: string) {
-    this.care_of = value;
-  }
+    /**
+     * Setter $care_of
+     * @param {string} value
+     */
+	public set $care_of(value: string) {
+		this.care_of = value;
+	}
 
-  /**
-   * Setter $gender
-   * @param {string} value
-   */
-  public set $gender(value: string) {
-    this.gender = value;
-  }
+    /**
+     * Setter $gender
+     * @param {string} value
+     */
+	public set $gender(value: string) {
+		this.gender = value;
+	}
 
-  /**
-   * Setter $name
-   * @param {string} value
-   */
-  public set $name(value: string) {
-    this.name = value;
-  }
+    /**
+     * Setter $maritalStatus
+     * @param {string} value
+     */
+	public set $maritalStatus(value: string) {
+		this.maritalStatus = value;
+	}
 
-  /**
-   * Setter $profession
-   * @param {string} value
-   */
-  public set $profession(value: string) {
-    this.profession = value;
-  }
+    /**
+     * Setter $profession
+     * @param {string} value
+     */
+	public set $profession(value: string) {
+		this.profession = value;
+	}
 
-  /**
-   * Setter $user_id
-   * @param {string} value
-   */
-  public set $user_id(value: string) {
-    this.user_id = value;
-  }
+    /**
+     * Setter $blood_Group
+     * @param {string} value
+     */
+	public set $blood_Group(value: string) {
+		this.blood_Group = value;
+	}
+
+    /**
+     * Setter $available
+     * @param {string} value
+     */
+	public set $available(value: string) {
+		this.available = value;
+	}
+
 
 
 }//class
