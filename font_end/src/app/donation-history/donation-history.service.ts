f140653684d771a8f3ac1057c1fc1a4dbb9fde1b
ajharul abedeen <ajharulabedeen@gmail.com>
2020-04-30 18:17:42 +0600

    f-history : getAll--working

--- font_end/src/app/donation-history/donation-history.service.ts
+++ font_end/src/app/donation-history/donation-history.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonationHistoryService {
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
 
-  constructor() { }
-}
+  public getHistory() {
+  }
+
+
+}// class
