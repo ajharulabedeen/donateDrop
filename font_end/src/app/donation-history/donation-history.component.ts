04df548528e26a7a2d05bf240b8ff2501ff819de
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-01 22:29:17 +0600

    f-history : showing history in table!

--- font_end/src/app/donation-history/donation-history.component.ts
+++ font_end/src/app/donation-history/donation-history.component.ts
@@ -17,6 +17,8 @@ export class DonationHistoryComponent implements OnInit {
   searchKey: string;
   public historyDonation: History[] = new Array();
 
+  // historyDonation = new Array();
+
   constructor(private serviceHistory: DonationHistoryService) {
   }
 
@@ -49,25 +51,23 @@ export class DonationHistoryComponent implements OnInit {
     srcRequest.start = '0';
     srcRequest.perPage = this.perPage;
     srcRequest.userID = '';
-    console.log(srcRequest);
+    // console.log(srcRequest);
     this.serviceHistory.getHistory(srcRequest).subscribe(res => {
       console.log(res);
       for (const key in res) {
         var his = new History();
-        his.$id = res['id'];
-        his.$date = res['date'];
-        his.$location = res['location'];
-        his.$note = res['note'];
-        his.$patient_description = res['patientDescription'];
-        his.$reffered_by = res['refferedBy'];
-        his.$user_id = res['userId'];
+        his.$id = res[key]['id'];
+        his.$date = res[key]['date'];
+        his.$location = res[key]['location'];
+        his.$note = res[key]['note'];
+        his.$patient_description = res[key]['patientDescription'];
+        his.$reffered_by = res[key]['refferedBy'];
+        his.$user_id = res[key]['userId'];
         this.historyDonation.push(his);
       }
     });
-
     // this.historyDonation = this.serviceHistory.getHistory(srcRequest);
     // console.log(this.historyDonation);
-
   }
 
 }
