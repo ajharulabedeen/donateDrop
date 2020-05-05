4cf1ecca48cabb52bd4272d4593b04ae00bb0a32
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-05 16:21:56 +0600

    b-agent-dao : interface created!

--- back_end/com.donatedrop/src/main/java/com/donatedrop/agent/Dao_Agent_I.java
+++ back_end/com.donatedrop/src/main/java/com/donatedrop/agent/Dao_Agent_I.java
@@ -1,5 +1,15 @@
 package com.donatedrop.agent;
 
+import java.util.Map;
+
 public interface Dao_Agent_I {
-    
+    /**
+     *
+     * @param agentRequest
+     * @return
+     * @apiNote for successful save, saved id will be back. OK status
+     */
+    public Map<String, String> saveRequest(AgentRequest agentRequest);
+    public Map<String, String> deleteRequest(AgentRequest agentRequest);
+
 }
