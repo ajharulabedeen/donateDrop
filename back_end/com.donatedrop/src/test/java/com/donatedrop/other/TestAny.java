8a36ba650cb46699e81dfa0de483c97df63f1ac4
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-06 09:38:17 +0600

    b-test-dump-data : add data into array format!

--- back_end/com.donatedrop/src/test/java/com/donatedrop/other/TestAny.java
+++ back_end/com.donatedrop/src/test/java/com/donatedrop/other/TestAny.java
@@ -13,12 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.List;
 import java.util.Random;
 
-@SpringBootTest
 public class TestAny {
-
-    @Autowired
-    private Dao_GeoCode_I dao_GeoCode_I;
-
 
     @Test
     public void testBCrypt() {
@@ -48,56 +43,11 @@ public class TestAny {
         }
     }
 
-    public String getAddress() {
-        Random r = new Random();
-//        System.out.println("\n" + dao_GeoCode_I.toString() + "\n");
-        String divID = "";
-        String divName = "";
-        String distID = "";
-        String distName = "";
-        String upzID = "";
-        String upzName = "";
-        String unionID = "";
-        String unionName = "";
-        try {
-            List<DivisionsEngName> divisionsList = dao_GeoCode_I.getDivisions();
-            int divRand = r.nextInt(divisionsList.size());
-            divID = divisionsList.get(divRand).getId().toString();
-            divName = divisionsList.get(divRand).getName().toString();
-        } catch (Exception e) {
-            System.out.println("Exception in Gettting Division!" + divID + "\n");
+    @Test
+    public void testGetAdd() {
+        for (int x = 0; x < 5; x++) {
+            System.out.println(DumpData.getAddress());
         }
-
-        try {
-            List<DistrictsEngName> districtsEngNameList = dao_GeoCode_I.getDistricts(divID);
-            int distRand = r.nextInt(districtsEngNameList.size());
-            distID = districtsEngNameList.get(distRand).getId().toString();
-            distName = districtsEngNameList.get(distRand).getName().toString();
-        } catch (Exception e) {
-            System.out.println("Exception in Gettting District! : " + distID + "\n");
-        }
-
-        try {
-            List<UpzillaEngName> upzillaEngNameList = dao_GeoCode_I.getUpzillas(distID);
-            int upzRand = r.nextInt(upzillaEngNameList.size());
-            upzID = upzillaEngNameList.get(upzRand).getId().toString();
-            upzName = upzillaEngNameList.get(upzRand).getName().toString();
-        } catch (Exception e) {
-            System.out.println("Exception in Gettting Upzilla! : " + upzID + "\n");
-        }
-
-        try {
-            List<UnionsEngName> unionsEngNameList = dao_GeoCode_I.getUnions(upzID);
-            int unionRand = r.nextInt(unionsEngNameList.size());
-            unionID = unionsEngNameList.get(unionRand).getId().toString();
-            unionName = unionsEngNameList.get(unionRand).getName().toString();
-        } catch (Exception e) {
-            System.out.println("Exception in Gettting Unions! : " + unionID + "\n");
-        }
-
-
-        return unionName + ", " + upzName + ", " + distName + ", " + divName;
     }
-
 
 }// class
