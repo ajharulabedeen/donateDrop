4614a5d9ab9bef7abae88c00bf1bc3ad6269a6f0
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-09 08:17:54 +0600

    f-agent-model : property add to model class!

---
+++ font_end/src/app/admin/agent/agent-request-to-review.model.spec.ts
@@ -0,0 +1,7 @@
+import { AgentRequestToReview } from './agent-request-to-review.model';
+
+describe('AgentRequestToReview', () => {
+  it('should create an instance', () => {
+    expect(new AgentRequestToReview()).toBeTruthy();
+  });
+});
---
+++ font_end/src/app/admin/agent/agent-request-to-review.model.ts
@@ -0,0 +1,35 @@
+import {PhoneNumber} from '../../profile/basic/phone-number.model';
+
+export class AgentRequestToReview {
+  public addressPermanent: string;
+  public addressPresent: string;
+
+  public gender: string;
+  public name: string;
+  note: string;
+
+  public permanentDist: string;
+  public permanentDiv: string;
+  public permanentId: string;
+  public permanentStreet: string;
+  public permanentUnion: string;
+  public permanentUpz: string;
+
+  public phone_number: PhoneNumber;
+
+  public presentDist: string;
+  public presentDiv: string;
+  public presentId: string;
+  public presentStreet: string;
+  public presentUnion: string;
+  public presentUpz: string;
+
+  public profession: string;
+  public profileId: string;
+  public requestDate: string;
+  public requestId: string;
+  public status: string;
+  public userId: string;
+  public username: string;
+
+}
