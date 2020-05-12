e06a23fc52dc3baa960ad58940e079b2e0c8c379
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-12 11:47:53 +0600

    f-agent-review : hold button remvove!

---
+++ font_end/src/app/admin/agent/freeze/agent-freeze.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-freeze',
+  templateUrl: './agent-freeze.component.html',
+  styleUrls: ['./agent-freeze.component.scss']
+})
+export class AgentFreezeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
