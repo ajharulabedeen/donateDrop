ec22d1a3fa7508e0b1f4b58c391844d69ecedaf2
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-12 09:40:29 +0600

    f-agent-review : hold  agent, add ui, component!

--- font_end/src/app/admin/agent/agent.component.ts
+++ font_end/src/app/admin/agent/agent.component.ts
@@ -28,6 +28,7 @@ export class AgentComponent implements OnInit {
     tablinks = document.getElementsByClassName('tablinks');
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(' active', '');
+      tablinks[i].style.backgroundColor = 'green';
     }
     document.getElementById(tabName).style.display = 'block';
     evt.currentTarget.className += ' active';
