b55a3f51842e6860e1160ab89cec7095e9173d9f
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-12 09:47:36 +0600

    f-agent :tab name hover color changed!

--- font_end/src/app/admin/agent/agent.component.ts
+++ font_end/src/app/admin/agent/agent.component.ts
@@ -28,10 +28,10 @@ export class AgentComponent implements OnInit {
     tablinks = document.getElementsByClassName('tablinks');
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(' active', '');
-      tablinks[i].style.backgroundColor = 'green';
     }
     document.getElementById(tabName).style.display = 'block';
     evt.currentTarget.className += ' active';
+    // evt.currentTarget.style.backgroundColor = 'pink';
   }
 
   // end : for tab in font end.
