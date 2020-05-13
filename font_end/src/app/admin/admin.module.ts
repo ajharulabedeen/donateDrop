4009f3193a3f654e280ef90497492e186e3ae39f
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-13 13:37:19 +0600

    files-help-plan : files, plan and help updated!

--- font_end/src/app/admin/admin.module.ts
+++ font_end/src/app/admin/admin.module.ts
@@ -5,10 +5,14 @@ import {AdminRoutingModule} from './admin-routing.module';
 import {AgentComponent} from './agent/agent.component';
 import {FormsModule, NgForm, ReactiveFormsModule} from '@angular/forms';
 import { ReviewComponent } from './agent/review/review.component';
+import { AgentAcceptedComponent } from './agent/accepted/agent-accepted.component';
+import { AgentRejectedComponent } from './agent/rejected/agent-rejected.component';
+import { AgentHoldComponent } from './agent/hold/agent-hold.component';
+import { AgentFreezeComponent } from './agent/freeze/agent-freeze.component';
 
 
 @NgModule({
-  declarations: [AgentComponent, ReviewComponent],
+  declarations: [AgentComponent, ReviewComponent, AgentAcceptedComponent, AgentRejectedComponent, AgentHoldComponent, AgentFreezeComponent],
   imports: [
     CommonModule,
     FormsModule,
