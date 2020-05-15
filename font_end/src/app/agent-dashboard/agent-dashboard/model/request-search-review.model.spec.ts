33580efd43eed3a861ee12e5da3fa2b2c99c1622
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-15 12:42:05 +0600

    f-agent-dashbord-model : model add!

---
+++ font_end/src/app/agent-dashboard/agent-dashboard/model/request-search-review.model.spec.ts
@@ -0,0 +1,7 @@
+import { RequestSearchReview } from './request-search-review.model';
+
+describe('RequestSearchReview', () => {
+  it('should create an instance', () => {
+    expect(new RequestSearchReview()).toBeTruthy();
+  });
+});
---
+++ font_end/src/app/agent-dashboard/agent-dashboard/model/request-search-review.model.ts
@@ -0,0 +1,7 @@
+export class RequestSearchReview {
+  public start: string;
+  public max: string;
+  public column: string;
+  public key: string;
+  public statusType: string;
+}
