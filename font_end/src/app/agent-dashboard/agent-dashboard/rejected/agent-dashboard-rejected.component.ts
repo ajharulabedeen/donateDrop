ab95662bfd425bc7504fe5252e8f4ec20922d706
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-15 17:09:39 +0600

    f-agent-dashboard-rejected: UI add!

---
+++ font_end/src/app/agent-dashboard/agent-dashboard/rejected/agent-dashboard-rejected.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-dashboard-rejected',
+  templateUrl: './agent-dashboard-rejected.component.html',
+  styleUrls: ['./agent-dashboard-rejected.component.scss']
+})
+export class AgentDashboardRejectedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
