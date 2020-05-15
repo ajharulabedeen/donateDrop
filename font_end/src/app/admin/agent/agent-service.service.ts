b27959329af0922a4e3b2b4228fd5dac33e9e566
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-15 09:43:53 +0600

    f-agent : agent model create!

--- font_end/src/app/admin/agent/agent-service.service.ts
+++ font_end/src/app/admin/agent/agent-service.service.ts
@@ -34,73 +34,20 @@ export class AgentServiceService {
 
   public requestReview(reviewRequestObj: RequestReviewRequest) {
     return this.http.post(
-      'http://localhost:8080/public/user/reviewRequest', reviewRequestObj,
+      'http://localhost:8080/public/user/agent/admin/reviewRequest', reviewRequestObj,
       this.authService.getHeader());
   }
 
   public updateAdminNote(adminNote: RequestAdminNote) {
     return this.http.post(
-      'http://localhost:8080/public/user/updateAdminNote', adminNote,
+      'http://localhost:8080/public/user/agent/admin/updateAdminNote', adminNote,
       this.authService.getHeader());
   }
 
   public updatePersonalNote(personalNote: RequestPersonalNote) {
     return this.http.post(
-      'http://localhost:8080/public/user/updatePersonalNote', personalNote,
+      'http://localhost:8080/public/user/agent/admin/updatePersonalNote', personalNote,
       this.authService.getHeader());
   }
-
-  // start : old code
-  // public getHistoryCount(searchRequest: SearchRequest) {
-  //   return this.http.post(
-  //     'http://127.0.0.1:8080/public/user/history/searchCount', searchRequest,
-  //     this.authService.getHeader());
-  // }
-  //
-  // public getHistory(srcRequest: SearchRequest): History[] {
-  //   // working
-  //   // return this.http.post(
-  //   //   'http://127.0.0.1:8080/public/user/history/search', srcRequest,
-  //   //   this.authService.getHeader());
-  //
-  //   var h: History[] = new Array();
-  //
-  //   this.http.post(
-  //     'http://127.0.0.1:8080/public/user/history/search', srcRequest,
-  //     this.authService.getHeader()).subscribe(res => {
-  //     console.log(res);
-  //     for (const key in res) {
-  //       var his = new History();
-  //       his.$id = res[key]['id'];
-  //       his.$date = res[key]['date'];
-  //       his.$location = res[key]['location'];
-  //       his.$note = res[key]['note'];
-  //       his.$patientDescription = res[key]['patientDescription'];
-  //       his.$refferedBy = res[key]['refferedBy'];
-  //       his.$user_id = res[key]['userId'];
-  //       h.push(his);
-  //     }
-  //   });
-  //   return h;
-  // }
-  //
-  // public save(history: History) {
-  //   return this.http.post(
-  //     'http://127.0.0.1:8080/public/user/history/save', history,
-  //     this.authService.getHeader());
-  //
-  //   //   .subscribe(res => {
-  //   //   console.log(res);
-  //   // });
-  // }
-  //
-  // public delete(deleteId: string) {
-  //   return this.http.post(
-  //     'http://127.0.0.1:8080/public/user/history/delete?historyID=' + deleteId, [],
-  //     this.authService.getHeader());
-  // }
-
-  // end : old code
-
 
 }
