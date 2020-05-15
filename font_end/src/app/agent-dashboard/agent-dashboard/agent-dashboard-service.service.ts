0841c42ea528fceb83b1c7bbb8cbb002a082cc98
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-15 12:38:28 +0600

    f-agent-dashboard-service : service class add and url writting!

--- font_end/src/app/agent-dashboard/agent-dashboard/agent-dashboard-service.service.ts
+++ font_end/src/app/agent-dashboard/agent-dashboard/agent-dashboard-service.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {RequestGetAgentRequests} from '../../admin/agent/models/request-get-agent-requests.model';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgentDashboardServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
+
+  // http://localhost:8080/public/user/agent/donner/getDonnerToAgentRequestReviewCount
+  // http://localhost:8080/public/user/agent/donner/getDonnerToAgentRequestToReview
+  public getDonnerToAgentRequestToReview(agentSearch: RequestGetAgentRequests) {
+    return this.http.post(
+      'http://localhost:8080/public/user/agent/admin/getAgentRequestsToReview', agentSearch,
+      this.authService.getHeader());
+  }
+
+  // http://localhost:8080/public/user/agent/donner/reviewDonnerRequest
+  // http://localhost:8080/public/user/agent/donner/updateAgentNote
+  // http://localhost:8080/public/user/agent/donner/updateNoteAgentPersonal
+
 }
