3122e9e84e532f801654f802f259c1da1a067b60
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-15 12:28:16 +0600

    f-agent-dashboard : from model add!

--- font_end/src/app/agent-dashboard/agent-dashboard.module.ts
+++ font_end/src/app/agent-dashboard/agent-dashboard.module.ts
@@ -2,12 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AgentDashboardReviewComponent} from './agent-dashboard/review/agent-dashboard-review.component';
 import {AgentDashboardComponent} from './agent-dashboard/agent-dashboard.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [AgentDashboardReviewComponent, AgentDashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AgentDashboardModule {
