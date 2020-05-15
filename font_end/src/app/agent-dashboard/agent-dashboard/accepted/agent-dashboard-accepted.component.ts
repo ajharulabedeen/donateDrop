0c1632dade59276949c2c9ce2d60ff09c7ac8773
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-15 16:17:21 +0600

    f-agent-dashboard-accepted: UI add!

---
+++ font_end/src/app/agent-dashboard/agent-dashboard/accepted/agent-dashboard-accepted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-dashboard-accepted',
+  templateUrl: './agent-dashboard-accepted.component.html',
+  styleUrls: ['./agent-dashboard-accepted.component.scss']
+})
+export class AgentDashboardAcceptedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
