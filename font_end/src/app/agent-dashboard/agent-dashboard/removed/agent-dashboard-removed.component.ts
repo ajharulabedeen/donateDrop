28fbfa709af18951773aacff43f2ad9f4cec96be
ajharul abedeen <ajharulabedeen@gmail.com>
2020-05-16 07:05:25 +0600

    f-agent-dashboard-removed : componet create and UI add!

---
+++ font_end/src/app/agent-dashboard/agent-dashboard/removed/agent-dashboard-removed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-dashboard-removed',
+  templateUrl: './agent-dashboard-removed.component.html',
+  styleUrls: ['./agent-dashboard-removed.component.scss']
+})
+export class AgentDashboardRemovedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
