144b287aaadb69b67dfcba25e9fd0ad0d71acb9d
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-06-29 09:47:09 +0600

    fix-agent-dashboard : remove component!

--- font_end/src/app/agent-dashboard/agent-dashboard.module.ts
+++ font_end/src/app/agent-dashboard/agent-dashboard.module.ts
@@ -3,13 +3,9 @@ import {CommonModule} from '@angular/common';
 import {AgentDashboardReviewComponent} from './agent-dashboard/review/agent-dashboard-review.component';
 import {AgentDashboardComponent} from './agent-dashboard/agent-dashboard.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { AgentDashboardAcceptedComponent } from './agent-dashboard/accepted/agent-dashboard-accepted.component';
-import { AgentDashboardRejectedComponent } from './agent-dashboard/rejected/agent-dashboard-rejected.component';
-import { AgentDashboardRemovedComponent } from './agent-dashboard/removed/agent-dashboard-removed.component';
-
 
 @NgModule({
-  declarations: [AgentDashboardReviewComponent, AgentDashboardComponent, AgentDashboardAcceptedComponent, AgentDashboardRejectedComponent, AgentDashboardRemovedComponent],
+  declarations: [AgentDashboardReviewComponent, AgentDashboardComponent],
   imports: [
     CommonModule,
     FormsModule,
