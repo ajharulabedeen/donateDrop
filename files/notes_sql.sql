b308258f2dda1349178161f2f8da22a1a7eb4bce
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-06-29 09:42:11 +0600

    files : old code keeping!

--- files/notes_sql.sql
+++ files/notes_sql.sql
@@ -1,4 +1,61 @@
-
+SELECT
+    *
+FROM
+    `address`,
+    `donner_to_agent_request_review`
+WHERE
+    donner_to_agent_request_review.profile_id = `address`.`profile_id` 
+    AND
+    (`address`.`division` LIKE '%khulna%' 
+     OR `address`.`district` LIKE '%khulna%' 
+     OR `address`.`upzilla` LIKE '%khulna%'
+     OR `address`.`union_ward` LIKE '%khulna%')
+=================================================
+SELECT
+    `profilebasic`.*,
+    `request_donner_to_agent`.*,
+    `user`.`USERNAME`
+FROM
+    `request_donner_to_agent`,
+    `profilebasic`,
+    `user`
+WHERE
+    `request_donner_to_agent`.`user_id_donner` = `profilebasic`.`user_id`
+    AND `request_donner_to_agent`.`user_id_donner`=`user`.`ID`
+=================================================
+SELECT
+    *
+FROM
+    `address`,
+    `agent_request_review`
+WHERE
+    agent_request_review.profile_id = `address`.`profile_id` 
+    AND
+    (`address`.`division` LIKE '%khulna%' 
+     OR `address`.`district` LIKE '%khulna%' 
+     OR `address`.`upzilla` LIKE '%khulna%'
+     OR `address`.`union_ward` LIKE '%khulna%')
+--------------------------------------------
+SELECT
+    `profilebasic`.*,
+    `agent_request`.`agent_request_id`,
+    `agent_request`.`accept_date`,
+    `agent_request`.`freeze_date`,
+    `agent_request`.`note_admin`,
+    `agent_request`.`note_applicant`,
+    `agent_request`.`note_personal`,
+    `agent_request`.`reject_date`,
+    `agent_request`.`request_date`,
+    `agent_request`.`status`,
+    `user`.`USERNAME`
+FROM
+    `agent_request`,
+    `profilebasic`,
+    `user`
+WHERE
+    `agent_request`.`user_id` = `profilebasic`.`user_id`
+    AND `agent_request`.`user_id`=`user`.`ID`
+--------------------------------------
 
 
 SELECT * FROM `profilebasic` WHERE (CAST(birth_date AS DATETIME) BETWEEN CAST('2020-05-10' AS DATETIME) AND CAST('2020-05-18' AS DATETIME))
