d52e056f21e067117b9e6a586d4f19324bd3b50d
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-08-19 18:29:29 +0600

    post-details : post basic field blood type!

--- font_end/src/app/post/post-details/post-details.component.ts
+++ font_end/src/app/post/post-details/post-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-post-details',
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailsComponent implements OnInit {
 
-  constructor() { }
+  public postID: string;
+  public needDate: string;
+  public bloodType: string;
+  public quantity: string;
+  public patientGender: string;
+  public relation: string;
+  public hospitalName: string;
+  public hospitalAddress: string;
+  public location: string;
+  public contactInfo: string;
+  public patientDescription: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
