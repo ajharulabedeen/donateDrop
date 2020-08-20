32c82cafa6c0e39f640308d9cf0ee61bd86c2b59
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-08-20 12:23:41 +0600

    post-details : comment basic with user info model class add!

---
+++ font_end/src/app/post/post-details/postcomment-with-user-info.model.spec.ts
@@ -0,0 +1,7 @@
+import { PostcommentWithUserInfo } from './postcomment-with-user-info.model';
+
+describe('PostcommentWithUserInfo', () => {
+  it('should create an instance', () => {
+    expect(new PostcommentWithUserInfo()).toBeTruthy();
+  });
+});
---
+++ font_end/src/app/post/post-details/postcomment-with-user-info.model.ts
@@ -0,0 +1,12 @@
+export class PostcommentWithUserInfo {
+  public commentId: string;
+  public commentDetails: string;
+  public commentDate: string;
+  public commentUserId: string;
+  public postId: string;
+  public name: string;
+  public email: string;
+  public gender: string;
+  public profession: string;
+  public commentOwner: string;
+}
