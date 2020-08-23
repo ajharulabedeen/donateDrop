5fb3ede6c724a77ad03fad4fed27a8a101c674e0
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-08-23 09:11:39 +0600

    donner-assign : DonnerAssingShow model create!

---
+++ font_end/src/app/agent-dashboard/agent-dashboard/model/donner-assing-show.model.spec.ts
@@ -0,0 +1,7 @@
+import { DonnerAssingShow } from './donner-assing-show.model';
+
+describe('DonnerAssingShow', () => {
+  it('should create an instance', () => {
+    expect(new DonnerAssingShow()).toBeTruthy();
+  });
+});
---
+++ font_end/src/app/agent-dashboard/agent-dashboard/model/donner-assing-show.model.ts
@@ -0,0 +1,22 @@
+export class DonnerAssingShow {
+  donnerAssingmentId: string;
+  agentId: string;
+  donnerId: string;
+  assingDate: string;
+  assingNote: string;
+  needDate: string;
+  postId: string;
+  bloodManageStatus: string;
+  privatelongprofileId: string;
+  available: string;
+  birthDate: string;
+  bloodGroup: string;
+  careOf: string;
+  email: string;
+  gender: string;
+  maritalStatus: string;
+  name: string;
+  profession: string;
+  religion: string;
+  userId: string;
+}
