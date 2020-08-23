03e4f4eaf4e71d582a522e2c11a45de7c47d7692
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-08-23 11:15:55 +0600

    donner-assign : getAllAssignment!

--- font_end/src/app/agent-dashboard/agent-dashboard/donner-assigning/donner-assign.service.ts
+++ font_end/src/app/agent-dashboard/agent-dashboard/donner-assigning/donner-assign.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthService} from '../../../auth/auth.service';
 import {RequestSearchReview} from '../model/request-search-review.model';
+import {RequestSearchDonnerAssing} from '../model/request-search-donner-assign.model';
+import {DonnerAssingShow} from '../model/donner-assing-show.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +19,7 @@ export class DonnerAssignService {
   //     this.authService.getHeader());
   // }
 
+  getAssingments(donnerAssingSearch: RequestSearchDonnerAssing) {
+    return this.http.post<DonnerAssingShow[]>('http://localhost:8080/public/user/agent/donnerAssign/getAssingments', donnerAssingSearch, this.authService.getHeader());
+  }
 }
--- font_end/src/app/agent-dashboard/agent-dashboard/donner-assigning/donner-assigning.component.ts
+++ font_end/src/app/agent-dashboard/agent-dashboard/donner-assigning/donner-assigning.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RequestSearchDonnerAssing} from '../model/request-search-donner-assign.model';
+import {DonnerAssingShow} from '../model/donner-assing-show.model';
+import {DonnerAssignService} from './donner-assign.service';
 
 @Component({
   selector: 'app-donner-assigning',
@@ -7,9 +9,6 @@ import {RequestSearchDonnerAssing} from '../model/request-search-donner-assign.m
   styleUrls: ['./donner-assigning.component.scss']
 })
 export class DonnerAssigningComponent implements OnInit {
-
-  constructor() {
-  }
 
   searchKey: string;
   searchByColumn = 'name';
@@ -20,16 +19,33 @@ export class DonnerAssigningComponent implements OnInit {
   statusType: string;
   assignmentStatus: string;
 
+  donnerAssingmentAll: Array<DonnerAssingShow>;
+
+  constructor(private donnerAssignService: DonnerAssignService) {
+  }
+
   ngOnInit() {
+    this.searchKey = '';
+    this.statusType = '0';
+    this.assignmentStatus = '0';
+    this.getAssingments();
   }
 
   public getAssingments(): void {
     const donnerAssingSearch = new RequestSearchDonnerAssing();
     donnerAssingSearch.start = this.startAssigments;
     donnerAssingSearch.max = this.perPage;
-    donnerAssingSearch.column = this.searchKey;
-    donnerAssingSearch.key = this.searchByColumn;
-    donnerAssingSearch.statusType =this.assignmentStatus;
+    donnerAssingSearch.column = this.searchByColumn;
+    donnerAssingSearch.key = '%' + this.searchKey + '%';
+    donnerAssingSearch.statusType = this.assignmentStatus;
+    this.donnerAssingmentAll = new Array<DonnerAssingShow>();
+
+    console.log(donnerAssingSearch);
+
+    this.donnerAssignService.getAssingments(donnerAssingSearch).subscribe(donnerAssignment => {
+      this.donnerAssingmentAll = donnerAssignment;
+      console.log(this.donnerAssingmentAll);
+    });
   }
 
 }
