af0292a4d4636a5c1c3f92db34b271c818aa00ce
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-08-24 22:24:24 +0600

    donner-assigment : post details router link from assigments!

--- font_end/src/app/agent-dashboard/agent-dashboard.module.ts
+++ font_end/src/app/agent-dashboard/agent-dashboard.module.ts
@@ -4,13 +4,15 @@ import {AgentDashboardReviewComponent} from './agent-dashboard/review/agent-dash
 import {AgentDashboardComponent} from './agent-dashboard/agent-dashboard.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { DonnerAssigningComponent } from './agent-dashboard/donner-assigning/donner-assigning.component';
+import {RouterLink, RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [AgentDashboardReviewComponent, AgentDashboardComponent, DonnerAssigningComponent],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ]
 })
 export class AgentDashboardModule {
