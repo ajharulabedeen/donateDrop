407851ff665c1b9cfde51c1105affd1aa5b2241e
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-08-25 09:37:16 +0600

    donner-assigment : profile detials model Add!

---
+++ font_end/src/app/model/profile-details.model.spec.ts
@@ -0,0 +1,7 @@
+import { ProfileDetails } from './profile-details.model';
+
+describe('ProfileDetails', () => {
+  it('should create an instance', () => {
+    expect(new ProfileDetails()).toBeTruthy();
+  });
+});
---
+++ font_end/src/app/model/profile-details.model.ts
@@ -0,0 +1,23 @@
+import {PhoneNumber} from '../profile/basic/phone-number.model';
+import {Address} from './address.model';
+import {EmergencyContact} from '../profile/basic/emergency-contact.model';
+
+export class ProfileDetails {
+  public id: string;
+  public userId: string;
+  public name: string;
+  public birthDate: string;
+  public care_of: string;
+  public gender: string;
+  public maritalStatus: string;
+  public profession: string;
+  public blood_Group: string;
+  public available: string;
+  public religion: string;
+  public email: string;
+  public profileFound: boolean;
+  public lastBloodGiven: string;
+  public phoneNumbers: Array<PhoneNumber>;
+  public address: Array<Address>;
+  public emergencyContact: Array<EmergencyContact>
+}
